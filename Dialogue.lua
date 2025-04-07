ecd2e1703f8e902b3aa0e2a02d6e91288a1faab2
bandi10xx <84464903+bandi10xx@users.noreply.github.com>
2025-04-07 21:26:53 +0200

    Update Dialogue.lua

--- Dialogue.lua
+++ Dialogue.lua
@@ -1,9 +1,14 @@
-Dialogue = {
-    "Welcome",
-    "To",
-    "Bandi's",
-    "Game!",
-    "I hope you enjoy it!",
+local Dialogue = {
+
+    Dialogue = {
+        "Welcome",
+        "To",
+        "Bandi's",
+        "Game!",
+        "I hope you enjoy it!",
+    }
+    
 }
+
 
 return Dialogue
